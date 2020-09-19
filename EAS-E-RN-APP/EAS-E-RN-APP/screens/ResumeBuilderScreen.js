19ae7aeb943b8a9ae67ba33a9c5c9010a0392e7f
backpack455 <39465356+backpack455@users.noreply.github.com>
2020-09-19 10:21:52 -0400

    More commits

--- EAS-E-RN-APP/EAS-E-RN-APP/screens/ResumeBuilderScreen.js
+++ EAS-E-RN-APP/EAS-E-RN-APP/screens/ResumeBuilderScreen.js
@@ -2,9 +2,9 @@ import React from 'react'
 import {StyleSheet} from 'react-native'
 import {WebView} from 'react-native-webview'
 
-const website = 'https://slate-forum.tribe.so/topics'
+const website = 'resolute-reading.surge.sh'
 
-export default class EnglishTool extends React.Component{
+export default class ResumeBuilder extends React.Component{
     render(){
         return(
             <WebView source={{ uri: website }}  />
